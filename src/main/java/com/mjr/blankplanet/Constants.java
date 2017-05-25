967de2d05993b1f4ed27f6aa5fcf420d3d104253
MJRLegends <MJRLegends@users.noreply.github.com>
2017-05-25 23:32:49 +0100

    Released v0.4 - 1.10.2

--- src/main/java/com/mjr/blankplanet/Constants.java
+++ src/main/java/com/mjr/blankplanet/Constants.java
@@ -3,7 +3,7 @@ package com.mjr.blankplanet;
 public class Constants {
 	public static final String modID = "BlankPlanet";
 	public static final String modName = "BlankPlanet";
-	public static final String modVersion = "0.3-1.10.2";
+	public static final String modVersion = "0.4-1.10.2";
 	
 	public static final String ASSET_PREFIX = "blankplanet";
     public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
