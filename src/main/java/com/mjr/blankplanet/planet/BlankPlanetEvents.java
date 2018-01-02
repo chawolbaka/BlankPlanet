368aa2ab3f8d9de4f6d28a739cb74c469994339d
MJRLegends <MJRLegends@users.noreply.github.com>
2018-01-02 15:30:07 +0000

    Renamed packages (2/2)

---
+++ src/main/java/com/mjr/blankplanet/planet/BlankPlanetEvents.java
@@ -0,0 +1,29 @@
+package com.mjr.blankplanet.planet;
+
+import micdoodle8.mods.galacticraft.api.event.oxygen.GCCoreOxygenSuffocationEvent;
+import micdoodle8.mods.galacticraft.core.event.EventWakePlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.mjr.blankplanet.BlankPlanet;
+
+public class BlankPlanetEvents {
+	@SubscribeEvent
+	public void GCCoreOxygenSuffocationEvent(GCCoreOxygenSuffocationEvent.Pre event) {
+		if (event.entityLiving.worldObj.provider.getDimensionId() == BlankPlanet.dimensionid) {
+			event.setCanceled(true);
+		}
+	}
+
+	@SubscribeEvent
+	public void GCCoreEventWakePlayer(EventWakePlayer event) {
+		if (event.entityLiving.worldObj.provider.getDimensionId() == BlankPlanet.dimensionid) {
+			event.entityPlayer.heal(5.0F);
+
+			for (WorldServer worldServer : MinecraftServer.getServer().worldServers) {
+				worldServer.setWorldTime(0);
+			}
+		}
+	}
+}
