eb00d6d51acbaaedd79c3af1e98365d3c3e3f3d8
MJRLegends <MJRLegends@users.noreply.github.com>
2018-08-17 13:04:08 +0100

    Tweaks for mod information to match my other mods layouts

--- src/main/java/com/mjr/blankplanet/Constants.java
+++ src/main/java/com/mjr/blankplanet/Constants.java
@@ -5,6 +5,12 @@ public class Constants {
 	public static final String modName = "BlankPlanet";
 	public static final String modVersion = "0.7-1.11.2";
 
+	public static final String MCVERSION = "[1.12.2]";
+	public static final String DEPENDENCIES_FORGE = "required-after:forge@(13.20.1.2513,);";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.11.2-1.0.2,);required-after:galacticraftcore;required-after:galacticraftplanets;";
+			
+	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
+
 	public static final String ASSET_PREFIX = "blankplanet";
 	public static final String TEXTURE_PREFIX = ASSET_PREFIX + ":";
 	public static final String PREFIX = "blankplanet.";
