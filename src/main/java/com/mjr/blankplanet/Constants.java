438e5da69154372402b1877b4dd91a5ab5a048c9
MJRLegends <MJRLegends@users.noreply.github.com>
2018-09-07 22:44:25 +0100

    Released v0.8, Changed requirement of MJRLegendsLib to 1.8.9-1.0.4 & above

--- src/main/java/com/mjr/blankplanet/Constants.java
+++ src/main/java/com/mjr/blankplanet/Constants.java
@@ -3,11 +3,11 @@ package com.mjr.blankplanet;
 public class Constants {
 	public static final String modID = "BlankPlanet";
 	public static final String modName = "BlankPlanet";
-	public static final String modVersion = "1.8.9-0.7";
+	public static final String modVersion = "1.8.9-0.8";
 
 	public static final String MCVERSION = "[1.8.9]";
 	public static final String DEPENDENCIES_FORGE = "required-after:Forge@[11.15.1.1764,);";
-	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.8.9-1.0.2,);required-after:GalacticraftCore;";
+	public static final String DEPENDENCIES_MODS = "required-after:mjrlegendslib@[1.8.9-1.0.4,);required-after:GalacticraftCore;";
 
 	public static final String CERTIFICATEFINGERPRINT = "b02331787272ec3515ebe63ecdeea0d746653468";
 
